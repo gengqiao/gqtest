bba5dcd36e15ec4dc8873ad109ce675004e4e5e6
gengqiao <gengqiaode@163.com>
2017-02-08 15:08:39 +0800

    增加了qq邮箱的发送

--- src/main/java/com/whaty/util/SendEmail.java
+++ src/main/java/com/whaty/util/SendEmail.java
@@ -10,6 +10,7 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 /** 
+ * 163邮箱发邮件
 * @className:SendEmail.java
 * @classDescription:
 * @author:gengqiao
