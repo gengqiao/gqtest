ea8146bfa779e6281056b2c2bad70cb026f58689
gengqiao <gengqiaode@163.com>
2017-02-15 17:23:38 +0800

    进行了一步封装，可以直接操作线上文件

--- src/main/java/com/gq/mysql/ChangeDB.java
+++ src/main/java/com/gq/mysql/ChangeDB.java
@@ -2,54 +2,87 @@ package com.gq.mysql;
 import com.jcraft.jsch.JSch;  
 import com.jcraft.jsch.Session;  
 import java.sql.*;  
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 /**
  *jdbc使用SSH隧道连接mysql数据库demo
  * @author gengqiao
  *可以运用和这个小程序直连数据库。处理数据
  */
 public class ChangeDB {  
-	static int lport = 3307;//本地端口  
-    static String host = "192.168.148.244";//远程MySQL服务器  
-    static int rport = 3306;//远程MySQL服务端口 
-    public static void go() {  
-        try {  
+    /**
+     * 
+   * 获得映射
+     * @param username 远程ssh的用户名
+     * @param password 远程ssh的密码
+     * @param ConnecHost 远程地址
+     * @param lport 本地需要映射的端口
+     * @param host 远程MySql服务器的内网地址 
+     * @param rport 远程MySQL服务端口 
+     * @param Database 数据库名
+     * @param DataUserName 数据库用户名
+     * @param DataPassword 数据库密码
+     * @return
+     */
+    public static Connection go(String username,String password,String ConnecHost,int lport, String host, int rport,String Database,String DataUserName,String DataPassword) {  
+    	Connection   conn=null;
+    	try {  
             JSch jsch = new JSch();  
-            Session session = jsch.getSession("readonly", "210.14.140.85", 22); // 端口账号 
-            session.setPassword("v1Riz8hTex");  
+            Session session = jsch.getSession(username, ConnecHost, 22); // 端口账号 
+            session.setPassword(password);  
             session.setConfig("StrictHostKeyChecking", "no");  
             session.connect();  
             System.out.println(session.getServerVersion());//这里打印SSH服务器版本信息  
             int assinged_port=  session.setPortForwardingL(lport, host, rport);//端口映射 转发
             System.out.println("localhost:" + assinged_port);  //端口映射 转发
+                //1、加载驱动  
+             Class.forName("com.mysql.jdbc.Driver");  
+             conn = DriverManager.getConnection("jdbc:mysql://localhost:"+lport+"/"+Database, DataUserName, DataPassword); 
         } catch (Exception e) {  
             e.printStackTrace();  
-        }  
+        } 
+        return conn;
+        
     }  
-    public static void main(String[] args) {  
-        try {  
-            //1、加载驱动  
-            Class.forName("com.mysql.jdbc.Driver");  
-        } catch (ClassNotFoundException e) {  
-            e.printStackTrace();  
-        }  
-        //2、创建连接  
-        Connection conn = null;  
-       // Connection conn2 = null;  
-        try{  
-            System.out.println("=============");  
-            go();  
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:"+lport+"/webtrn", "webtrn", "N8RWrda3"); 
-            System.out.println("--------------");
-            getData(conn);  
-        } catch (SQLException e) {  
-            e.printStackTrace();  
-        }  
+/**
+ * 数据查询的方法
+ * @param conn 数据库连接
+ * @param sql 需要查询的sql
+ * @return 一个以sql的列名为key的map组成的list
+ */
+    public static List getData(Connection conn,String sql)  {  
+    	List list=new ArrayList();
+    	try {
+	        // 获取所有表名  
+	        Statement statement = conn.createStatement();  
+	        ResultSet resultSet = statement.executeQuery(sql);  
+	        // 获取列名  
+	        ResultSetMetaData metaData = resultSet.getMetaData();
+	        // 获取数据  
+	        while (resultSet.next()) {
+	        	Map map=new HashMap();
+	            for (int i = 0; i < metaData.getColumnCount(); i++) {  
+	                // resultSet数据下标从1开始  
+	            	String columnName = metaData.getColumnName(i + 1);  
+	            	String value=resultSet.getString(i + 1);
+	            	map.put(columnName, value);
+	            }  
+	            list.add(map);
+	        }  
+	        statement.close();  
+	        conn.close();  
+    	} catch (SQLException e) {
+			// TODO: handle exception
+		} 
+        return list;
     }  
   
     private static void getData(Connection conn) throws SQLException {  
         // 获取所有表名  
         Statement statement = conn.createStatement();  
-        ResultSet resultSet = statement.executeQuery("select * from enum_const");  
+        ResultSet resultSet = statement.executeQuery("select * from enum_const limit 10");  
         // 获取列名  
         ResultSetMetaData metaData = resultSet.getMetaData();  
         for (int i = 0; i < metaData.getColumnCount(); i++) {  
